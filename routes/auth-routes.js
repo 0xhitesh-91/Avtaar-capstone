dbe3da09e7f398f05908d7f643c627e007a5f495
hitesh <17bcs1666@cuchd.in>
2022-02-16 02:48:30 +0530

    added middleware to authenticate access token, added functionality for logout and geting new accessToken

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,9 +1,8 @@
 import express from "express";
 import pool from "../db.js";
 import bcrypt from "bcrypt";
-import jsonwebtoken from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import { jwtTokens } from "../utils/jwt-helpers.js";
-//import {jwtTokens} form "../utils/jwt-helpers.js";
 
 const router = express.Router();
 
@@ -28,6 +27,29 @@ router.post('/login', async (req, res) => {
     }
 })
 
+router.get('/refresh_token', (req, res) => {
+    try {
+        const refreshToken = req.cookies.refresh_token;
+        if(refreshToken == null) return res.status(401).json({error : "Null refresh token"});
+        jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (error, user) => {
+            if(error) return res.status(403).json({error : error.message});
+            let tokens = jwtTokens(user);
+            res.cookie('refresh_token', tokens.refreshToken, {httpOnly: true});
+            res.json(tokens);
+        })
+    } catch (error) {
+        res.status(401).json({error : error.message});
+    }
+})
+
+router.delete('/refresh_token', (req, res) => {
+    try {
+        res.clearCookie('refresh_token');
+        return res.status(200).json({message: "Refresh token deleted"});
+    } catch (error) {
+        res.status(401).json({error : error.message});
+    }
+})
 
 
 export default router;
